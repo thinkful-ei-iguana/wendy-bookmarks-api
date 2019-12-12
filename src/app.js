8aabf729d43eaa9edc7378639af9b459013c2bc7
Sophia Koeut <koeutsophia@gmail.com>
2019-12-12 11:23:51 -0800

    made bookmark-services

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
 const { NODE_ENV } = require("./config");
+const BookmarksService = require('./bookmarks-service')
 const app = express();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
@@ -11,6 +12,10 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.get('/bookmarks', (req, res, next) => {
+  res.send('All bookmarks')
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
